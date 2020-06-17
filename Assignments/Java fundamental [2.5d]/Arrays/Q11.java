2585afbf5a3bfa89e06f466f884018af7312505c
Raj Hans <49719208+RajHans01@users.noreply.github.com>
2020-06-18 00:49:16 +0530

    Create Q11.java

---
+++ Assignments/Java fundamental [2.5d]/Arrays/Q11.java
@@ -0,0 +1,31 @@
+Given an array of type int, print true if every element is 1 or 4. 
+
+only14([1, 4, 1, 4]) → true
+only14([1, 4, 2, 4]) → false
+only14([1, 1]) → true
+=================================================================
+
+import java.util.Scanner;
+
+public class Q11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int a[] = new int[n];
+		boolean flag = true;
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+		for (int i : a) {
+			if (i != 1 && i != 4) {
+				flag = false;
+				break;
+			}
+		}
+		System.out.println(flag);
+
+	}
+
+}
