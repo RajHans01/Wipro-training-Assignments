612c2c1c5e90f6206cbb3438e1da41f88e47bcac
Raj Hans <49719208+RajHans01@users.noreply.github.com>
2020-06-17 16:42:27 +0530

    Create Q11.java

---
+++ Assignments/Java fundamental [2.5d]/Flow Control Statments/Q11.java
@@ -0,0 +1,18 @@
+
+/*Write a program to print even numbers between 23 and 57. 
+Each number should be printed in a separate row.
+======================================================================*/
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Q11 {
+	public static void main(String args[]) {
+		for(int i =24;i<=56;i+=2) {
+			System.out.println(i);
+		}
+	}
+
+}
